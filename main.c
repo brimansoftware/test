62ac77ded95bdbb551d98e3fbfe9badc7d4ce714
Brian Franco <support@brimansoftware.com>
2020-03-15 10:54:01 -0700

    Just testing
    Testing

---
+++ main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+
+int main(int argc, char *argv[])
+
+{
+
+int firstNumber;
+int secondNumber; 
+int addNumbers;
+
+printf("Please enter first number:\n");
+
+scanf("%d", &firstNumber);
+
+printf("Please enter second number: \n");	
+
+scanf("%d", &secondNumber);
+
+addNumbers = firstNumber + secondNumber;
+
+printf("Your 2 numbers added together are: %d \n", addNumbers);
+
+
+
+return 0;
+
+
+
+
+
+}
